7d8a5ba580f8c2c022680712c7f46ea1bffbbb80
DeepCFoxXx <learncode23@gmail.com>
2018-03-25 17:12:40 +0100

    2nd bounty added

--- db/console.rb
+++ db/console.rb
@@ -9,3 +9,11 @@ bounty1 = Bounty.new({
   'last_known_location' => 'Kamino'
   })
   bounty1.save()
+
+  bounty2 = Bounty.new({
+    'name' => 'IG88',
+    'bounty_value' => 5000,
+    'danger_level' => 'Medium',
+    'last_known_location' => 'Halowan'
+    })
+    bounty2.save()
